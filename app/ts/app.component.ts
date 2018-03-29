cce97a662d01016f60f40befac99f069b30e1e4d
Nazanin Sabri <nazanin.sabrii@gmail.com>
2018-03-29 22:19:54 +0430

    finished tutorial

--- app/ts/app.component.ts
+++ app/ts/app.component.ts
@@ -1,13 +1,23 @@
 import {Component} from 'angular2/core';
 import {config} from './config.service';
 import {Video} from './video';
-import {PlaylistComponent} from './playlist.component'
+import {PlaylistComponent} from './playlist.component';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>{{ mainHeading }}</h1>'
+    templateUrl: 'app/ts/app.component.html',
+    directives: [PlaylistComponent]
 })
 
 export class AppComponent {
 	mainHeading = config.MAIN_HEADING;
+	videos: Array<Video>;
+
+
+	constructor(){
+		this.videos = [
+			new Video (1, "Dancing Takes Center Stage on the Set of 'NCIS: LA'", "xbhcqDEWbyc", "dancing on NCIS"), 
+			new Video (2, "NCIS: LA Daniela Ruah & Eric Christian Olsen interview", "BxqAlB72d3U", "NCIS london")
+		]
+	}
 }
